c529f56d3f44512c25538548b713f4eae55dab51
Christoph Pilka <c.pilka@asconix.com>
2014-09-09 00:12:48 +0200

    Minor changes

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule OpenProxyScanner.Mixfile do
   def project do
     [ app: :open_proxy_scanner,
       version: "0.0.1",
-      elixir:  "~> 1.0.0-rc1",
+      elixir:  "~> 1.0.0-rc2",
       elixirc_paths: ["lib", "web"],
       deps: deps ]
   end
@@ -16,7 +16,6 @@ defmodule OpenProxyScanner.Mixfile do
       applications: [
         :inets,
         :plug,
-        :plug_basic_auth,
         :phoenix,
         :cowboy,
         :logger
@@ -34,9 +33,8 @@ defmodule OpenProxyScanner.Mixfile do
       {:plug, "~> 0.7.0"},
       {:cowboy, "~> 1.0.0"},
       {:phoenix, "~> 0.4.0"},
-      {:postgrex, "~> 0.5.3"},
-      {:ecto, github: "elixir-lang/ecto"},
-      {:plug_basic_auth, "~> 0.2.0"}
+      {:postgrex, "~> 0.6.0"},
+      {:ecto, "~> 0.2.4"}
     ]
   end
 end
